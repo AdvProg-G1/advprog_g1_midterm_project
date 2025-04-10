d7f4b85517f2176ebb5ffda159d4beb8f8298b50
a23barho <azim.barhoumi@imt-atlantique.net>
2025-04-11 00:06:36 +0700

    [GREEN] Implement FixedDiscountCouponFactory and PercentageDiscountCouponFactory to pass unit tests

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Coupon/factory/FixedDiscountCouponFactory.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Coupon/factory/FixedDiscountCouponFactory.java
@@ -1,12 +1,12 @@
 package id.ac.ui.cs.advprog.perbaikiinaja.Coupon.factory;
 
 import id.ac.ui.cs.advprog.perbaikiinaja.Coupon.model.Coupon;
+import id.ac.ui.cs.advprog.perbaikiinaja.Coupon.model.FixedDiscountCoupon;
 
-public class FixedDiscountCouponFactory implements CouponFactory{
+public class FixedDiscountCouponFactory implements CouponFactory {
 
-	@Override
-	public Coupon createCoupon(String code, double value, int maxUsage) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Coupon createCoupon(String code, double value, int maxUsage) {
+        return new FixedDiscountCoupon(code, value, maxUsage);
+    }
 }
--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Coupon/factory/PercentageDiscountCouponFactory.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Coupon/factory/PercentageDiscountCouponFactory.java
@@ -1,12 +1,12 @@
 package id.ac.ui.cs.advprog.perbaikiinaja.Coupon.factory;
 
 import id.ac.ui.cs.advprog.perbaikiinaja.Coupon.model.Coupon;
+import id.ac.ui.cs.advprog.perbaikiinaja.Coupon.model.PercentageDiscountCoupon;
 
-public class PercentageDiscountCouponFactory implements CouponFactory{
+public class PercentageDiscountCouponFactory implements CouponFactory {
 
-	@Override
-	public Coupon createCoupon(String code, double value, int maxUsage) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Coupon createCoupon(String code, double value, int maxUsage) {
+        return new PercentageDiscountCoupon(code, value, maxUsage);
+    }
 }
