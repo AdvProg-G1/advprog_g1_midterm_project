f63fe43ae108124d78a6d3805664fdb4c24fea47
AndhikaNayakaAW <andhika.nayaka@ui.ac.id>
2025-04-10 22:06:38 +0700

    [RED] Define RatingStrategy Interface for Overall Rating Calculation (Strategy Design Pattern)

---
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Review/strategy/SimpleAverageRatingStrategy.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.perbaikiinaja.Review.strategy;
+
+import id.ac.ui.cs.advprog.perbaikiinaja.Review.model.Review;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+// Skeleton implementation (to be fully implemented in GREEN commit)
+@Component
+public class SimpleAverageRatingStrategy implements RatingStrategy {
+
+    @Override
+    public double calculateRating(List<Review> reviews) {
+        // TODO: implement simple average calculation
+        return 0;
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Review/strategy/WeightedRatingStrategy.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.perbaikiinaja.Review.strategy;
+
+import id.ac.ui.cs.advprog.perbaikiinaja.Review.model.Review;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+// Skeleton implementation (to be fully implemented in GREEN commit)
+@Component
+public class WeightedRatingStrategy implements RatingStrategy {
+
+    @Override
+    public double calculateRating(List<Review> reviews) {
+        // TODO: implement weighted average calculation
+        return 0;
+    }
+}
