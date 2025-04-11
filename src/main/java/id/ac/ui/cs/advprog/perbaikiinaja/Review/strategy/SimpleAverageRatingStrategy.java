ed04095827908d08f6c2ae2318813e8f104602e6
AndhikaNayakaAW <andhika.nayaka@ui.ac.id>
2025-04-11 15:54:05 +0700

    [GREEN] Mark SimpleAverageRatingStrategy as primary to resolve ambiguous dependency injection in OverallRatingService

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Review/strategy/SimpleAverageRatingStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Review/strategy/SimpleAverageRatingStrategy.java
@@ -1,9 +1,12 @@
+// src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Review/strategy/SimpleAverageRatingStrategy.java
 package id.ac.ui.cs.advprog.perbaikiinaja.Review.strategy;
 
 import id.ac.ui.cs.advprog.perbaikiinaja.Review.model.Review;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import java.util.List;
 
+@Primary
 @Component
 public class SimpleAverageRatingStrategy implements RatingStrategy {
 
