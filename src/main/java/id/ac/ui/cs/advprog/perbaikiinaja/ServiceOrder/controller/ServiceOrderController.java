ebe9d7f0e3bb960ce1fea89955002b05da57efa8
Brian <briansubekti11@gmail.com>
2025-04-11 15:36:21 +0700

    [REFACTOR] Update ServiceOrderController

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/ServiceOrder/controller/ServiceOrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/ServiceOrder/controller/ServiceOrderController.java
@@ -1,32 +1,46 @@
 package id.ac.ui.cs.advprog.perbaikiinaja.ServiceOrder.controller;
 
+import id.ac.ui.cs.advprog.perbaikiinaja.ServiceOrder.dto.CreateServiceOrderRequest;
 import id.ac.ui.cs.advprog.perbaikiinaja.ServiceOrder.model.ServiceOrder;
 import id.ac.ui.cs.advprog.perbaikiinaja.ServiceOrder.service.ServiceOrderService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/service-orders")
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class ServiceOrderController {
 
-    @Autowired
-    private ServiceOrderService serviceOrderService;
+    private final ServiceOrderService serviceOrderService;
 
-    @PostMapping("/create")
-    public ServiceOrder createServiceOrder(@RequestBody ServiceOrder order) {
-        return serviceOrderService.createOrder(order);
+    @PostMapping
+    public ResponseEntity<ServiceOrder> createOrder(@RequestBody CreateServiceOrderRequest request) {
+        ServiceOrder order = ServiceOrder.builder()
+                .itemName(request.getItemName())
+                .condition(request.getCondition())
+                .problemDescription(request.getProblemDescription())
+                .technicianId(request.getTechnicianId())
+                .userId("user1") // Temp hardcoded
+                .serviceDate(request.getServiceDate())
+                .paymentMethod(request.getPaymentMethod())
+                .couponApplied(request.isCouponApplied())
+                .build();
+
+        return ResponseEntity.ok(serviceOrderService.createOrder(order));
     }
 
-    @GetMapping("/")
-    public List<ServiceOrder> getAllOrders() {
-        return serviceOrderService.getAllOrders();
+    @GetMapping
+    public ResponseEntity<List<ServiceOrder>> getAllOrders() {
+        return ResponseEntity.ok(serviceOrderService.getAllOrders());
     }
 
     @GetMapping("/{id}")
-    public ServiceOrder getOrderById(@PathVariable UUID id) {
-        return serviceOrderService.getOrderById(id);
+    public ResponseEntity<ServiceOrder> getOrderById(@PathVariable UUID id) {
+        ServiceOrder order = serviceOrderService.getOrderById(id);
+        return order != null ? ResponseEntity.ok(order) : ResponseEntity.notFound().build();
     }
 }
