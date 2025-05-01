0f1d539a4c26fc48b889b6b7c7c56c21cf8ea604
luticakep <kaylasorayadj@gmail.com>
2025-05-01 23:45:42 +0700

    [GREEN] Modify controller with some endpoints

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/ConfirmService/controller/RepairOrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/ConfirmService/controller/RepairOrderController.java
@@ -2,8 +2,11 @@ package id.ac.ui.cs.advprog.perbaikiinaja.ConfirmService.controller;
 
 import id.ac.ui.cs.advprog.perbaikiinaja.ConfirmService.model.RepairOrder;
 import id.ac.ui.cs.advprog.perbaikiinaja.ConfirmService.service.RepairOrderService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/repair")
@@ -28,4 +31,17 @@ public class RepairOrderController {
         RepairOrder rejected = repairOrderService.rejectRepairOrder(orderId);
         return ResponseEntity.ok(rejected);
     }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<RepairOrder>> getAllRepairOrders() {
+        List<RepairOrder> allOrders = repairOrderService.getAllRepairOrders();
+        return ResponseEntity.ok(allOrders);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<RepairOrder> createRepairOrder(@RequestBody RepairOrder order) {
+        RepairOrder created = repairOrderService.createRepairOrder(order);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
 }
