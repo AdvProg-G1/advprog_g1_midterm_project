56e453f54e00e4164a665b7b50d4f1d4d17766da
a23barho <azim.barhoumi@imt-atlantique.net>
2025-05-02 18:10:01 +0700

    [GREEN] Filling the CouponController methods to successfully pass the tests

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Coupon/controller/CouponController.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/Coupon/controller/CouponController.java
@@ -1,21 +1,13 @@
 package id.ac.ui.cs.advprog.perbaikiinaja.Coupon.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import id.ac.ui.cs.advprog.perbaikiinaja.Coupon.dto.CouponRequest;
 import id.ac.ui.cs.advprog.perbaikiinaja.Coupon.dto.CouponResponse;
 import id.ac.ui.cs.advprog.perbaikiinaja.Coupon.service.CouponService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/coupons")
@@ -26,26 +18,28 @@ public class CouponController {
 
     @PostMapping
     public ResponseEntity<CouponResponse> createCoupon(@RequestBody CouponRequest request) {
-        return null;
+        CouponResponse created = couponService.createCoupon(request);
+        return ResponseEntity.status(201).body(created);
     }
 
     @GetMapping
     public ResponseEntity<List<CouponResponse>> getAllCoupons() {
-        return null;
+        return ResponseEntity.ok(couponService.getAllCoupons());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CouponResponse> getCouponById(@PathVariable("id") String id) {
-        return null;
+        return ResponseEntity.ok(couponService.getCouponById(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<CouponResponse> updateCoupon(@PathVariable("id") String id, @RequestBody CouponRequest request) {
-        return null;
+        return ResponseEntity.ok(couponService.updateCoupon(id, request));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCoupon(@PathVariable("id") String id) {
-        return null;
+        couponService.deleteCoupon(id);
+        return ResponseEntity.noContent().build();
     }
 }
