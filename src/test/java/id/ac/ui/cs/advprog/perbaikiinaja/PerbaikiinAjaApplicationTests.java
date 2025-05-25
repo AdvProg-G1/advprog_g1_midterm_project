8d9a1ebe008e16b0970485eee12a3c3d554a787c
AndhikaNayakaAW <andhika.nayaka@ui.ac.id>
2025-05-25 17:27:36 +0700

    [GREEN] CORE: enable bean-definition overriding in PerbaikiinAjaApplicationTests

--- src/test/java/id/ac/ui/cs/advprog/perbaikiinaja/PerbaikiinAjaApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/perbaikiinaja/PerbaikiinAjaApplicationTests.java
@@ -1,13 +1,18 @@
-//src/test/java/id/ac/ui/cs/advprog/perbaikiinaja/PerbaikiinAjaApplicationTests.java
+// src/test/java/id/ac/ui/cs/advprog/perbaikiinaja/PerbaikiinAjaApplicationTest.java
 package id.ac.ui.cs.advprog.perbaikiinaja;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PerbaikiinAjaApplicationTests {
+@SpringBootTest(
+        classes = PerbaikiinAjaApplication.class,
+        properties = "spring.main.allow-bean-definition-overriding=true"
+)
+class PerbaikiinAjaApplicationTest {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+        // If the application context fails to start, this will fail.
+    }
 
 }
